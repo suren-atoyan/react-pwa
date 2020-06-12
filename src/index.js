f085afc4e1ac3a5caeb006da333427ebfdab6d08
Suren Atoyan <contact@surenatoyan.com>
2020-06-12 19:46:52 +0400

    do code splitting and sw integration with store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import App from './App';
+import store from 'store';
 
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (!document.ie) { // check for ie snippet
+  Promise.all([
+    import('react'),
+    import('react-dom'),
+    import('./App'),
+  ]).then(([
+    { default: React },
+    { default: ReactDOM },
+    { default: App },
+  ]) => {
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root'),
+    );
+  });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  serviceWorker.register({
+    onSuccess: store.actions.sw.handleSuccess,
+    onUpdate: reg => store.actions.sw.handleUpdate(reg),
+  });
+}
