dd18b5ceb3c7ec450689554c281be930da6eb7f3
Suren Atoyan <contact@surenatoyan.com>
2020-06-17 00:07:13 +0400

    fix reset icon path

--- src/errorHandling/Fallbacks/App/Component.js
+++ src/errorHandling/Fallbacks/App/Component.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import ResetIcon from '@material-ui/icons/Refresh';
+
+import { FaRedo as ResetIcon } from 'react-icons/fa';
 
 import { resetApp } from 'utils';
 import { messages, email } from 'config';
