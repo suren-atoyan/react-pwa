ca77cfd3120af6f74bd16306d32b1032c99bc9c7
Suren Atoyan <contact@surenatoyan.com>
2020-06-17 00:07:49 +0400

    create base structure of application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+
+import withErrorHandler from 'errorHandling';
+import { App as ErrorBoundaryFallback } from 'errorHandling/Fallbacks';
+
+import Layout from 'sections/Layout';
+import { ThemeProvider } from 'theme';
+import { StoreProvider } from 'store';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
-    <div>React-PWA</div>
+    <StoreProvider>
+      <ThemeProvider>
+        <Box display="flex">
+          <CssBaseline />
+          <Router>
+            <Layout />
+          </Router>
+        </Box>
+      </ThemeProvider>
+    </StoreProvider>
   );
 }
 
-export default App;
+export default withErrorHandler(App, ErrorBoundaryFallback);
