e3ccc1dd659d01a59f698c374b2408fcd794d95e
Suren Atoyan <contact@surenatoyan.com>
2020-06-17 17:17:55 +0400

    crate simple static hoster

---
+++ hoster/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8083;
+
+const indexTmplPath = path.join(__dirname, '../build/index.html');
+
+async function init() {
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, '../build/')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexTmplPath);
+  });
+
+  app.listen(port);
+}
+
+init()
+  .then(() => console.info(`Listening on port ${port}`))
+  .catch(err => console.error('Something went wrong', err));
