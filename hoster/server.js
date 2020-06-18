e3b983da9fe9677a1ac9fab4a690ca334118c582
Suren Atoyan <contact@surenatoyan.com>
2020-06-18 15:05:37 +0400

    change server port

--- hoster/server.js
+++ hoster/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 8083;
+const port = process.env.PORT || 8089;
 
 const indexTmplPath = path.join(__dirname, '../build/index.html');
 
