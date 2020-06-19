6f1d95c791eb468118202ee9b33afbfa178f7f41
Suren Atoyan <contact@surenatoyan.com>
2020-06-20 01:52:05 +0400

    fix typo (loading -> loading)

--- src/components/AsyncComponentLoader/Component.js
+++ src/components/AsyncComponentLoader/Component.js
@@ -2,9 +2,9 @@ import React, { Suspense } from 'react';
 
 import Loading from 'components/Loading';
 
-const AsyncComponentLoader = (Component, loadinProps) => props => {
+const AsyncComponentLoader = (Component, loadingProps) => props => {
   return (
-    <Suspense fallback={<Loading {...loadinProps} />}>
+    <Suspense fallback={<Loading {...loadingProps} />}>
       <Component {...props} />
     </Suspense>
   );
