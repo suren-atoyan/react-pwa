3813e734710e8f693aa23e4923d7a1c824be877b
Suren Atoyan <contact@surenatoyan.com>
2020-06-22 02:18:28 +0400

    set custom cache control in hoster

--- hoster/server.js
+++ hoster/server.js
@@ -5,10 +5,27 @@ const port = process.env.PORT || 8089;
 
 const indexTmplPath = path.join(__dirname, '../build/index.html');
 
+function setCustomCacheControl(res, path) {
+  const hashRegExp = new RegExp('\\.[0-9a-f]{8}\\.');
+
+  if (path.endsWith('.html')) {
+    // Never cache index.html or other .html files
+    res.setHeader('Cache-Control', 'no-cache');
+  } else if (hashRegExp.test(path)) {
+    // Always cache files with "fingerprint", they will never change
+    // like 0.c2572769.chunk.js
+    res.setHeader('Cache-Control', 'max-age=31536000');
+  }
+}
+
 async function init() {
   const app = express();
 
-  app.use(express.static(path.join(__dirname, '../build/')));
+  app.use(express.static(path.join(__dirname, '../build/'), {
+    etag: true,
+    lastModified: true, 
+    setHeaders: setCustomCacheControl,
+  }));
 
   app.get('*', (req, res) => {
     res.sendFile(indexTmplPath);
