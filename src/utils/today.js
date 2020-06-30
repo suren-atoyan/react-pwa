54632781ca0db4ad1fce4aa5364850784aef683c
Suren Atoyan <contact@surenatoyan.com>
2020-06-30 18:59:27 +0400

    fix code style

--- src/utils/today.js
+++ src/utils/today.js
@@ -2,8 +2,6 @@ import dayjs from 'dayjs';
 
 import { dateFormat } from 'config';
 
-const today = _ => {
-  return dayjs().format(dateFormat);
-};
+const today = _ => dayjs().format(dateFormat);
 
 export default today;
