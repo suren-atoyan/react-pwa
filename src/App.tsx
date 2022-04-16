58c196668228483a9c3b9912d94c9199c49d7788
suren-atoyan <contact@surenatoyan.com>
2022-04-16 23:05:04 +0400

    extract essential providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Fragment } from 'react';
-import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -13,27 +11,20 @@ import HotKeys from '@/sections/HotKeys';
 import Notifications from '@/sections/Notifications';
 import SW from '@/sections/SW';
 import Sidebar from '@/sections/Sidebar';
-import ThemeProvider from '@/theme/Provider';
 
 function App() {
   return (
-    <RecoilRoot>
-      <HelmetProvider>
-        <ThemeProvider>
-          <Fragment>
-            <CssBaseline />
-            <Notifications />
-            <HotKeys />
-            <SW />
-            <BrowserRouter>
-              <Header />
-              <Sidebar />
-              <Pages />
-            </BrowserRouter>
-          </Fragment>
-        </ThemeProvider>
-      </HelmetProvider>
-    </RecoilRoot>
+    <Fragment>
+      <CssBaseline />
+      <Notifications />
+      <HotKeys />
+      <SW />
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <Pages />
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
