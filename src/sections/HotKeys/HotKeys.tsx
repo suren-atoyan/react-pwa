9f1c1da2deadb40d67d29b05eddb9d86a0bfda4f
suren-atoyan <contact@surenatoyan.com>
2022-04-16 23:21:58 +0400

    explain non-defined hotkeys

--- src/sections/HotKeys/HotKeys.tsx
+++ src/sections/HotKeys/HotKeys.tsx
@@ -16,6 +16,10 @@ function HotKeys() {
   const [, sidebarActions] = useSidebar();
   const [isHotKeysDialogOpen, hotKeysDialogActions] = useHotKeysDialog();
 
+  // I would love to define all hotkeys in the config and loop it here and avoid this repetitive code.
+  // But the `react-hotkeys-hook` library, which we use to handle hotkeys provides only hook (`useHotkeys`).
+  // And as you know we can't use hooks inside loops (read "Rules of Hooks" - https://reactjs.org/docs/hooks-rules.html).
+  // There is always a workaround, but sometimes it's better to avoid premature and unnecessary optimizations :)
   useHotkeys('alt+s', themeActions.toggle);
   useHotkeys('alt+t', sidebarActions.toggle);
   useHotkeys('alt+/', hotKeysDialogActions.toggle);
