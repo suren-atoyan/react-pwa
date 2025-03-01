44f9204cd9e7b98ec66776b0126e99fa66fed447
Suren Atoyan <contact@surenatoyan.com>
2025-03-01 23:31:19 +0400

    fix CI env issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,19 +36,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
 
     /* Test against mobile viewports. */
     // {
@@ -73,7 +64,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: process.env.CI ? 'npm run build && npm run preview' : 'npm run dev',
+    command: process.env.CI
+      ? 'NODE_ENV=production npm run ts:check && NODE_ENV=production npm run build && NODE_ENV=production npm run preview'
+      : 'npm run dev',
     url: `http://localhost:${PORT}`,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
